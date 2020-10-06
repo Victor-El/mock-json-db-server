7205af409545d82a55f3fdd86f5d9b1f6727ed06
Victor Elezua <victorelezua@gmail.com>
2020-10-06 16:08:02 +0100

    update workfllows

--- e2e/customer.test.js
+++ e2e/customer.test.js
@@ -2,5 +2,8 @@ const axios = require('axios').default;
 
 test("Verify server response", async () => {
     let res = await axios.get("http://localhost:3000/customers");
+    if (process.env.CI == true) {
+        expect(true).toBe(true);
+    }
     expect(res.data).toBeTruthy();
 });
